f8979a79d91fc41e4d28605762c2a2203cd0769c
LarsLutz <32195663+LarsLutz@users.noreply.github.com>
2018-11-19 16:25:55 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+$method =$_SERVER['REQUEST_METHOD'];
+
+
+if($method =="POST"){
+	$requestBody =file_get_contents('php://input');
+	$json =json_decode($requestBody);
+	
+	$text=$jason ->result->parameters->Topping;
+	
+	switch ($text){
+		
+		case "Ham":
+		$speech ="I order a ham pizza";
+		break;
+		
+		case "cheese":
+		$speech ="I order a cheese pizza";
+		break;
+		
+		case "Vegi":
+		$speech ="I order a vegi pizza";
+		break;
+		
+		default:
+		$speech="Sorry, I havent this type of pizza.";
+		break;
+		
+	}
+	
+	$response =new \stdClass();
+	$response ->speech ="";
+	$response->displayText="";
+	$response-> source="webhook";
+	echo json_encode($response);
+	
+}
+else
+{
+	echo "Methode nicht erlaubt";
+}
+
+
+?>
