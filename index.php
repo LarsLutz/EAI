3e9b96e9daee51be53f389c715d987874d8f9e7b
LarsLutz <32195663+LarsLutz@users.noreply.github.com>
2018-11-19 16:46:36 +0100

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method =="POST"){
 	$requestBody =file_get_contents('php://input');
 	$json =json_decode($requestBody);
 	
-	$text=$jason ->result->parameters->Topping;
+	$text=$json ->queryResult->parameters->Topping;
 	
 	switch ($text){
 		
