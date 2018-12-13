668d6f8d1b77299f703ec705dd8fa5ed3657e171
robinwidmer17 <32195633+robinwidmer17@users.noreply.github.com>
2018-12-13 11:52:39 +0100

    Add files via upload

---
+++ Eshop/src/main/java/org/hameister/HelloWorldRestApplication.java
@@ -0,0 +1,30 @@
+package org.hameister;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+ 
+import java.util.Collection;
+ 
+@SpringBootApplication
+public class HelloWorldRestApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldRestApplication.class, args);
+    }
+}
+ 
+ 
+@RestController
+class HelloWorld {
+ 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<String>("Hello World!", HttpStatus.OK);
+    }
+}
